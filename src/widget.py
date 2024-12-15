a39998b64444442c1df6ec2b38539a49a5812d7e
Michael Vyazov <spres-vyaz@yandex.ru>
2024-12-15 16:09:23 +0300

    добавлена функция вывода даты

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(card_number: str) -> str:
-    "Функция принимает на вход строку, содержащую тип и номер карты или счета"
+    """Функция принимает на вход строку, содержащую тип и номер карты или счета"""
     splitted_list = card_number.split()
     if splitted_list[0] == "Счет":
         splitted_list[-1] = get_mask_account(splitted_list[-1])
@@ -11,5 +11,8 @@ def mask_account_card(card_number: str) -> str:
         splitted_list[-1] = get_mask_card_number(splitted_list[-1])
         return " ".join(splitted_list)
 
-
+def get_date(date_string: str) -> str:
+    """Функция принимает на вход строку с датой в виде ГГГГ-ММ-ДДТЧЧ:ММ:СС... и возвращает дату формата ДД.ММ.ГГГГ"""
+    splitted_list = date_string[:10].split("-")
+    return splitted_list[2] + "." + splitted_list[1] + "." + splitted_list[0]
 
