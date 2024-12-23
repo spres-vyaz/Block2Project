b2d34ad2aab6c8cb44cd343b4bb5a27012bff3a1
Michael Vyazov <spres-vyaz@yandex.ru>
2024-12-23 12:29:09 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,16 @@
 def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     """
     Функция принимает на вход список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
-    Функция возвращает новый список, отсортированный по дате
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению.
     :param list_of_dicts:
     :param state:
-    :return:
+    :return: new_list_of_dicts
     """
 
-    new_list = []
+    new_list_of_dicts = []
     for dict in list_of_dicts:
         if dict["state"] == state:
-            new_list.append(dict)
+            new_list_of_dicts.append(dict)
 
-    return new_list
+    return new_list_of_dicts
