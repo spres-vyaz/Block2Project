8cd2239a91fe221b57dd2e729a9c7c66b212b729
Michael Vyazov <spres-vyaz@yandex.ru>
2024-12-23 12:33:46 +0300

    добавлена функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,8 @@
 def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     """
-    Функция принимает на вход список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
+    Функция принимает на вход список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению.
-    :param list_of_dicts:
-    :param state:
-    :return: new_list_of_dicts
     """
 
     new_list_of_dicts = []
@@ -14,3 +11,12 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
             new_list_of_dicts.append(dict)
 
     return new_list_of_dicts
+
+def sort_by_date(list_of_dicts: list, reverse: bool = True) -> list:
+    """
+    Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению.
+    """
+
+    sorted_list_of_dicts = sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse)
+    return sorted_list_of_dicts
