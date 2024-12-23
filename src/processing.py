043f21722011d658bc1cdb1df968ad5dee1c17dd
Michael Vyazov <spres-vyaz@yandex.ru>
2024-12-23 19:55:01 +0300

    smal fixes

--- src/processing.py
+++ src/processing.py
@@ -6,16 +6,17 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     """
 
     new_list_of_dicts = []
-    for dict in list_of_dicts:
-        if dict["state"] == state:
+    for item in list_of_dicts:
+        if item["state"] == state:
             new_list_of_dicts.append(dict)
 
     return new_list_of_dicts
 
+
 def sort_by_date(list_of_dicts: list, reverse: bool = True) -> list:
     """
     Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
-    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению.
+    Функция возвращает возвращать новый список, отсортированный по дате (date).
     """
 
     sorted_list_of_dicts = sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse)
