641d5bbac2b342051135480ab7b4b5eb25c4c6a1
Michael Vyazov <spres-vyaz@yandex.ru>
2024-12-30 15:49:43 +0300

    добавлены тесты для модуля widget и некоторые фиксы

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 import pytest
 
 
-@pytest.mark.parametrize('number, expected', [
+@pytest.mark.parametrize("number, expected", [
     (7000792289606361, "7000 79** **** 6361"),
     (70007922896063617000792289606361, "7000 79** **** **** **** **** **** 6361"),
     ("8703216546546598asdaf", "Неверный формат данных. Введите число, а не строку"),
